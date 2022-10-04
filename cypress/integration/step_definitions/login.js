ec00cb7e51671ad7abffd80c25ba75109df5ab53
vivek-consensys <vivek.srilal@consensys.net>
2022-10-04 11:11:26 +0100

    POM implemented

--- cypress/integration/step_definitions/login.js
+++ cypress/integration/step_definitions/login.js
@@ -1,19 +1,20 @@
 import { Given, When, Then, Before } from "cypress-cucumber-preprocessor/steps";
+const homeSaucePage = require ('../../pages/homeSaucePage')
 
 Given('A user opens a login page', () => {
     cy.visit('/')
   })
 
   When('A user enters the username {string}', (username) => {
-    cy.get('#user-name').type(username)
+    homeSaucePage.typeUsername(username)
   })
 
   When('A user enters the password {string}', (password) => {
-    cy.get('#password').type(password)
+    homeSaucePage.typePassword(password)
   })
 
   And('A user clicks on the login button', () => {
-    cy.get('#login-button').click()
+    homeSaucePage.clickLogin()
   })
 
   Then('A user will be logged in', () => {
@@ -21,9 +22,9 @@ Given('A user opens a login page', () => {
   })
 
   Then('A user will be receiving an incorrect login message', ()=>{
-    cy.get('h3').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+    homeSaucePage.elements.errorMessage().should('have.text', 'Epic sadface: Username and password do not match any user in this service')
   })
 
   Then('A user will be receiving a locked out user message', ()=>{
-    cy.get('h3').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+    homeSaucePage.elements.errorMessage().should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
   })
